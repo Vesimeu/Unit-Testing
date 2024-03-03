99552cc96a1829654460a9b8c99fe41f2239e2d9
Vesimeu <pyoter.poliakov@yandex.ru>
2024-03-03 15:27:38 +0500

    Первый тест для 1 задания

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,4 +16,53 @@ public class CalculatorTest {
         // Assert
         Assert.assertEquals(result, 8.0, "The addition result is incorrect.");
     }
+
+    @Test
+    public void testSubtract() {
+        // Arrange
+        double a = 5;
+        double b = 3;
+    
+        // Act
+        double result = Calculator.subtract(a, b);
+    
+        // Assert
+        Assert.assertEquals(result, 2.0, "The subtraction result is incorrect.");
+    }
+
+    @Test
+    public void testMultiply() {
+        // Arrange
+        double a = 5;
+        double b = 3;
+    
+        // Act
+        double result = Calculator.multiply(a, b);
+    
+        // Assert
+        Assert.assertEquals(result, 15.0, "The multiplication result is incorrect.");
+    }
+
+    @Test
+    public void testDivide() {
+        // Arrange
+        double a = 10;
+        double b = 2;
+    
+        // Act
+        double result = Calculator.divide(a, b);
+    
+        // Assert
+        Assert.assertEquals(result, 5.0, "The division result is incorrect.");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        // Arrange
+        double a = 10;
+        double b = 0;
+    
+        // Act
+        Calculator.divide(a, b);
+    }
 }
